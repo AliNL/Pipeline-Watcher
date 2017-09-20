27afb96860e91a470b0aba3e9f4b9ef73084b9e5
Ying Zhou <yingzhou@thoughtworks.com>
2017-09-20 18:00:39 +0800

    remove drag and drop function

--- errors_tickets_watcher.py
+++ errors_tickets_watcher.py
@@ -54,50 +54,47 @@ class ErrorsTicketsWindow(object):
 
     def set_person_today(self):
         for dev in self.dev_list:
-            self.app.setLabelBg("dev" + dev, "white")
-            self.app.setLabelFg("dev" + dev, "black")
-            self.app.setLabelCursor("dev" + dev, "arrow")
+            self.app.setLabelBg(dev, "white")
+            self.app.setLabelFg(dev, "black")
+
         for bqa in self.bqa_list:
-            self.app.setLabelBg("bqa" + bqa, "white")
-            self.app.setLabelFg("bqa" + bqa, "black")
-            self.app.setLabelCursor("bqa" + bqa, "arrow")
-        self.app.unbindKey("<ButtonPress-1>")
-        self.app.unbindKey("<ButtonRelease-1>")
-        self.app.setLabelBg("dev" + self.dev_today, "#535353")
-        self.app.setLabelFg("dev" + self.dev_today, "white")
-        self.app.setLabelBg("bqa" + self.bqa_today, "#535353")
-        self.app.setLabelFg("bqa" + self.bqa_today, "white")
-        self.app.setLabelDragFunction("dev" + self.dev_today, [self.drag, self.drop])
-        self.app.setLabelDragFunction("bqa" + self.bqa_today, [self.drag, self.drop])
+            self.app.setLabelBg(bqa, "white")
+            self.app.setLabelFg(bqa, "black")
 
-    def drag(self, widget):
-        self.app.setLabelBg(widget, "white")
-        self.app.setLabelFg(widget, "black")
-        self.drag_from = widget
+        self.app.setLabelBg(self.dev_today, "#535353")
+        self.app.setLabelFg(self.dev_today, "white")
+        self.app.setLabelBg(self.bqa_today, "#535353")
+        self.app.setLabelFg(self.bqa_today, "white")
 
-    def drop(self, widget):
-        if self.drag_from[:3] == widget[:3]:
-            self.app.setLabelBg(widget, "#535353")
-            self.app.setLabelFg(widget, "white")
-            self.app.setLabelCursor(self.drag_from, "arrow")
-            self.app.unbindKey("<ButtonPress-1>")
-            self.app.unbindKey("<ButtonRelease-1>")
-            self.app.setLabelDragFunction(widget, [self.drag, self.drop])
 
-            if widget[:3] == 'dev':
-                dev_start = workdays.workday(self.today, -self.dev_list.index(widget[3:]))
-                self.data['dev_start_day'] = [dev_start.year, dev_start.month, dev_start.day]
-                self.dev_start_day = date(*self.data['dev_start_day'])
-            elif widget[:3] == 'bqa':
-                bqa_start = workdays.workday(self.today, -self.bqa_list.index(widget[3:]))
-                self.data['bqa_start_day'] = [bqa_start.year, bqa_start.month, bqa_start.day]
-                self.bqa_start_day = date(*self.data['bqa_start_day'])
-
-            with open(self.bundle_dir + '/data.json', 'w') as fw:
-                json.dump(self.data, fw)
-        else:
-            self.app.setLabelBg(self.drag_from, "#535353")
-            self.app.setLabelFg(self.drag_from, "white")
+    # def drag(self, widget):
+    #     self.app.setLabelBg(widget, "white")
+    #     self.app.setLabelFg(widget, "black")
+    #     self.drag_from = widget
+    #
+    # def drop(self, widget):
+    #     if self.drag_from[:3] == widget[:3]:
+    #         self.app.setLabelBg(widget, "#535353")
+    #         self.app.setLabelFg(widget, "white")
+    #         self.app.setLabelCursor(self.drag_from, "arrow")
+    #         self.app.unbindKey("<ButtonPress-1>")
+    #         self.app.unbindKey("<ButtonRelease-1>")
+    #         self.app.setLabelDragFunction(widget, [self.drag, self.drop])
+    #
+    #         if widget[:3] == 'dev':
+    #             dev_start = workdays.workday(self.today, -self.dev_list.index(widget[3:]))
+    #             self.data['dev_start_day'] = [dev_start.year, dev_start.month, dev_start.day]
+    #             self.dev_start_day = date(*self.data['dev_start_day'])
+    #         elif widget[:3] == 'bqa':
+    #             bqa_start = workdays.workday(self.today, -self.bqa_list.index(widget[3:]))
+    #             self.data['bqa_start_day'] = [bqa_start.year, bqa_start.month, bqa_start.day]
+    #             self.bqa_start_day = date(*self.data['bqa_start_day'])
+    #
+    #         with open(self.bundle_dir + '/data.json', 'w') as fw:
+    #             json.dump(self.data, fw)
+    #     else:
+    #         self.app.setLabelBg(self.drag_from, "#535353")
+    #         self.app.setLabelFg(self.drag_from, "white")
 
     def update_person(self):
         self.get_person_today()
@@ -117,10 +114,10 @@ class ErrorsTicketsWindow(object):
         self.app.setPadding(10, 30)
 
         for i in range(len(self.dev_list)):
-            self.app.addLabel("dev" + self.dev_list[i], self.dev_list[i], 0, i, 1, 1)
+            self.app.addLabel(self.dev_list[i], self.dev_list[i], 0, i, 1, 1)
 
         for i in range(len(self.bqa_list)):
-            self.app.addLabel("bqa" + self.bqa_list[i], self.bqa_list[i], 1, i, 1, 1)
+            self.app.addLabel(self.bqa_list[i], self.bqa_list[i], 1, i, 1, 1)
 
         self.app.setAllLabelWidths(15)
         self.app.setAllLabelHeights(1)

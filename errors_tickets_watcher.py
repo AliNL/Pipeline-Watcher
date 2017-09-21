b7905224554040da956b4703c7ecba305b0e4355
Ying Zhou <yingzhou@thoughtworks.com>
2017-09-21 15:04:45 +0800

    add stand up host to watcher & add move function

--- errors_tickets_watcher.py
+++ errors_tickets_watcher.py
@@ -17,40 +17,45 @@ class ErrorsTicketsWindow(object):
         self.bqa_list = self.data['bqa_list']
         self.dev_start_day = date(*self.data['dev_start_day'])
         self.bqa_start_day = date(*self.data['bqa_start_day'])
-        self.dev_today = None
-        self.bqa_today = None
-        self.today = datetime.today().date()
-        self.get_person_today()
+        self.host_start_day = date(*self.data['host_start_day'])
+        self.dev_today = 0
+        self.bqa_today = 0
+        self.host_today = 0
+        self.today = None
         self.app = gui("Errors & Tickets")
-        self.drag_from = None
         self.w = self.app.topLevel.winfo_screenwidth()
         self.h = self.app.topLevel.winfo_screenheight()
         self.pipelines = open_browser(self.data, self.w, self.h)
         self.browsers_watching = threading.Thread(target=watch, args=[self.pipelines, self.bundle_dir])
         self.browsers_watching.start()
 
+    def save_person_today(self):
+        with open(self.bundle_dir + '/data.json', 'w') as fw:
+            json.dump(self.data, fw)
+        return True
+
     def get_person_today(self):
         self.today = datetime.today().date()
         dev_workdays = workdays.networkdays(self.dev_start_day, self.today) - 1
         bqa_workdays = workdays.networkdays(self.bqa_start_day, self.today) - 1
-        dev_idx = dev_workdays % len(self.dev_list)
-        bqa_idx = bqa_workdays % len(self.bqa_list)
-        self.dev_today = self.dev_list[dev_idx]
-        self.bqa_today = self.bqa_list[bqa_idx]
+        host_workdays = workdays.networkdays(self.host_start_day, self.today) - 1
+        self.dev_today = dev_workdays % len(self.dev_list)
+        self.bqa_today = bqa_workdays % len(self.bqa_list)
+        self.host_today = host_workdays % (len(self.dev_list) + len(self.bqa_list))
 
-        update = False
-        if dev_idx == 0 and self.data['dev_start_day'][2] != self.today.day:
+        if self.dev_today == 0 and self.data['dev_start_day'][2] != self.today.day:
             self.data['dev_start_day'] = [self.today.year, self.today.month, self.today.day]
             self.dev_start_day = date(*self.data['dev_start_day'])
-            update = True
-        if bqa_idx == 0 and self.data['bqa_start_day'][2] != self.today.day:
+
+        if self.bqa_today == 0 and self.data['bqa_start_day'][2] != self.today.day:
             self.data['bqa_start_day'] = [self.today.year, self.today.month, self.today.day]
             self.bqa_start_day = date(*self.data['bqa_start_day'])
-            update = True
 
-        if update:
-            with open(self.bundle_dir + '/data.json', 'w') as fw:
-                json.dump(self.data, fw)
+        if self.host_today == 0 and self.data['host_start_day'][2] != self.today.day:
+            self.data['host_start_day'] = [self.today.year, self.today.month, self.today.day]
+            self.bqa_start_day = date(*self.data['host_start_day'])
+
+        self.save_person_today()
 
     def set_person_today(self):
         for dev in self.dev_list:
@@ -61,40 +66,36 @@ class ErrorsTicketsWindow(object):
             self.app.setLabelBg(bqa, "white")
             self.app.setLabelFg(bqa, "black")
 
-        self.app.setLabelBg(self.dev_today, "#535353")
-        self.app.setLabelFg(self.dev_today, "white")
-        self.app.setLabelBg(self.bqa_today, "#535353")
-        self.app.setLabelFg(self.bqa_today, "white")
+        host = self.dev_list[self.host_today] \
+            if self.host_today < len(self.dev_list) \
+            else self.bqa_list[self.host_today - len(self.dev_list)]
 
+        self.app.setLabelBg(self.dev_list[self.dev_today], "#535353")
+        self.app.setLabelFg(self.dev_list[self.dev_today], "white")
+        self.app.setLabelBg(self.bqa_list[self.bqa_today], "#535353")
+        self.app.setLabelFg(self.bqa_list[self.bqa_today], "white")
+        self.app.setLabelFg(host, "#FFBA00")
 
-    # def drag(self, widget):
-    #     self.app.setLabelBg(widget, "white")
-    #     self.app.setLabelFg(widget, "black")
-    #     self.drag_from = widget
-    #
-    # def drop(self, widget):
-    #     if self.drag_from[:3] == widget[:3]:
-    #         self.app.setLabelBg(widget, "#535353")
-    #         self.app.setLabelFg(widget, "white")
-    #         self.app.setLabelCursor(self.drag_from, "arrow")
-    #         self.app.unbindKey("<ButtonPress-1>")
-    #         self.app.unbindKey("<ButtonRelease-1>")
-    #         self.app.setLabelDragFunction(widget, [self.drag, self.drop])
-    #
-    #         if widget[:3] == 'dev':
-    #             dev_start = workdays.workday(self.today, -self.dev_list.index(widget[3:]))
-    #             self.data['dev_start_day'] = [dev_start.year, dev_start.month, dev_start.day]
-    #             self.dev_start_day = date(*self.data['dev_start_day'])
-    #         elif widget[:3] == 'bqa':
-    #             bqa_start = workdays.workday(self.today, -self.bqa_list.index(widget[3:]))
-    #             self.data['bqa_start_day'] = [bqa_start.year, bqa_start.month, bqa_start.day]
-    #             self.bqa_start_day = date(*self.data['bqa_start_day'])
-    #
-    #         with open(self.bundle_dir + '/data.json', 'w') as fw:
-    #             json.dump(self.data, fw)
-    #     else:
-    #         self.app.setLabelBg(self.drag_from, "#535353")
-    #         self.app.setLabelFg(self.drag_from, "white")
+    def move_dev_list(self, btn):
+        self.dev_today = (self.dev_today + 1) % len(self.dev_list)
+        dev_start = workdays.workday(self.today, -self.dev_today)
+        self.data['dev_start_day'] = [dev_start.year, dev_start.month, dev_start.day]
+        self.dev_start_day = date(*self.data['dev_start_day'])
+        self.set_person_today()
+
+    def move_bqa_list(self, btn):
+        self.bqa_today = (self.bqa_today + 1) % len(self.bqa_list)
+        bqa_start = workdays.workday(self.today, -self.bqa_today)
+        self.data['bqa_start_day'] = [bqa_start.year, bqa_start.month, bqa_start.day]
+        self.bqa_start_day = date(*self.data['bqa_start_day'])
+        self.set_person_today()
+
+    def move_host_list(self, btn):
+        self.host_today = (self.host_today + 1) % (len(self.dev_list) + len(self.bqa_list))
+        host_start = workdays.workday(self.today, -self.host_today)
+        self.data['host_start_day'] = [host_start.year, host_start.month, host_start.day]
+        self.host_start_day = date(*self.data['host_start_day'])
+        self.set_person_today()
 
     def update_person(self):
         self.get_person_today()
@@ -112,17 +113,26 @@ class ErrorsTicketsWindow(object):
         self.app.setGuiPadding(50, 40)
         self.app.setFont(32)
         self.app.setPadding(10, 30)
+        i = j = 0
 
         for i in range(len(self.dev_list)):
             self.app.addLabel(self.dev_list[i], self.dev_list[i], 0, i, 1, 1)
 
-        for i in range(len(self.bqa_list)):
-            self.app.addLabel(self.bqa_list[i], self.bqa_list[i], 1, i, 1, 1)
+        for j in range(len(self.bqa_list)):
+            self.app.addLabel(self.bqa_list[j], self.bqa_list[j], 1, j, 1, 1)
+
+        self.app.addLabel("help", """按 d 移动Dev\n按 b 移动BA/QA\n按 h 移动Host""", 1, j + 1, i - j, 1)
+        self.app.setLabelFg("help", "#A6A6A6")
+        self.app.getLabelWidget("help").config(font=12)
 
         self.app.setAllLabelWidths(15)
         self.app.setAllLabelHeights(1)
         self.app.setStretch("row")
         self.app.setSticky(None)
+        self.app.bindKey('<d>', self.move_dev_list)
+        self.app.bindKey('<b>', self.move_bqa_list)
+        self.app.bindKey('<h>', self.move_host_list)
+        self.app.setStopFunction(self.save_person_today)
         self.app.registerEvent(self.update_person)
         self.app.setPollTime(600000)
         self.app.go()
